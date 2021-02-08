877ba036fb5c6d051274ba55e9575ce8e208c19b
kanou.s <kanou.s@tam-tam.co.jp>
2021-02-09 08:05:06 +0900

    solve prop drilling issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
 import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Event from "./Event";
-import Events from "./Events";
-import reducer from "../reducers";
 import EventForm from "./EventForm";
+import Events from "./Events";
+import AppContext from "../contexts/AppContext";
+import reducer from "../reducers";
+
+console.log({ AppContext });
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}/>
-      <Events state={state} dispatch={dispatch}/>
-    </div>
+    <AppContext.Provider value={{ state, dispatch }}>
+      <div className="container-fluid">
+        <EventForm />
+        <Events />
+      </div>
+    </AppContext.Provider>
   );
 };
 
