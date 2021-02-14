7f274e7810dd4d04c3972252940d7fad9200411e
kanou.s <kanou.s@tam-tam.co.jp>
2021-02-14 09:42:06 +0900

    delete operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -3,7 +3,7 @@ import {
   CREATE_EVENT,
   DELETE_ALL_EVENTS,
   ADD_OPERATION_LOG,
-  DELETE_ALLOPERATION_LOGS,
+  DELETE_ALL_OPERATION_LOGS,
 } from "../actions";
 import AppContext from "../contexts/AppContext";
 import { timeCurrentIso8601 } from "../utils";
@@ -46,6 +46,17 @@ const EventForm = () => {
   };
 
   const unCreatable = title === "" || body === "";
+
+  const deleteAllOparationLogs = e => {
+    e.preventDefault()
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？');
+    if(result) {
+      dispatch({
+        type: DELETE_ALL_OPERATION_LOGS
+      })
+    }
+  }
+
   return (
     <>
       <h4>イベント作成フォーム</h4>
@@ -83,6 +94,13 @@ const EventForm = () => {
           disabled={state.events.length === 0}
         >
           全てのイベントを削除する
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllOparationLogs}
+          disabled = {state.operationLogs.length === 0}
+        >
+          全ての操作ログを削除する
         </button>
       </form>
     </>
